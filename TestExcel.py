7ead3ddb4c0ec78794c53da3a5b35718af341bfa
InfiniteZone <35384629+InfiniteZone@users.noreply.github.com>
2020-02-09 22:11:17 -0600

    Add files via upload

--- TestExcel.py
+++ TestExcel.py
@@ -1,8 +1,15 @@
 # Author: Tanner Nystrom
+from tkinter import filedialog
+from tkinter import *
 import xlrd
-# the "r" is needed at the beginning of the path so python reads it correct (not as a string)
-# stores file location in loc
-loc = r"D:\Projects\Python\Test.xlsx"
+
+
+# Prompt user to select file for reading of type Excel or All
+root = Tk()
+root.fileName = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("Excel files","*.xlsx"),("All files","*.*")))
+
+
+loc = root.fileName
 
 # opens the workbook
 wb = xlrd.open_workbook(loc)
@@ -11,12 +18,24 @@ wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
 
 # creates an array to store all values
-cells = []
+cellsNumber = []
+cellsString = []
 # loop through every cell, add it to the array
 for i in range(0, sheet.nrows):
     for j in range(0, sheet.ncols):
         # sheet.cell_value calls the exact value (EX: 2.0) but sheet.cell calls value and type (EX: number: 2.0)
-        cells.append(int(sheet.cell_value(i, j)))
+        if isinstance(sheet.cell_value(i, j), str):
+            cellsString.append(sheet.cell_value(i, j))
+        else:
+            cellsNumber.append(int(sheet.cell_value(i, j)))
 
 # print every value in the array cells
-print(cells)
+print ("Numbers found in file " + str(cellsNumber))
+print ("Strings found in file " + str(cellsString))
+
+cellsNumber.sort()
+cellsString.sort()
+print("================================================")
+print ("Sorted Numbers found in file " + str(cellsNumber))
+print ("Alphabetical Strings found in file " + str(cellsString))
+
