bd14316fb50a85269f35bff8dacd8ad866b3fa4e
Trevor Knutson <trevor@dyn177-103.wireless-1176.nat.ndsu.NoDak.edu>
2020-03-02 10:38:22 -0600

    Updated graphing and initialized midterm-dev branch

--- dataVisualization.py
+++ dataVisualization.py
@@ -1,25 +1,44 @@
+
+
 import sys
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import *
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import numpy as np
-from PyQt5.QtWidgets import *
 
 
-class GraphWithQtDemo(QWidget):
-    def __init__(self, parent = None):
-        super(GraphWithQtDemo, self).__init__(parent)
+class Window(QWidget):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
         self.layout = QGridLayout()
-        self.title = QLabel('<h1>Graph Demo</h1>', parent=self)
-        self.m = PlotCanvas(self, width=5, height=4)
-        self.m.move(0, 0)
+        self.title = QLabel('<h1>Bar Graph Demo</h1>', parent=self)
+        self.graph = PlotCanvas(self, width=5, height=4)
+        self.graph.move(0, 0)
+        button = QPushButton('Push me!', self)
+        button.clicked.connect(self.on_click)
 
-        self.button = QPushButton('PyQt5 button', self)
-        self.button.setToolTip('This s an example button')
+        self.layout.addWidget(button, 3, 3)
         self.layout.addWidget(self.title, 0, 0)
-        self.layout.addWidget(self.m, 1, 0)
-        self.layout.addWidget(self.button, 3, 3)
+        self.layout.addWidget(self.graph, 1, 0)
 
         self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        show_dialog()
+
+
+def show_dialog():
+    message = QMessageBox()
+    message.setIcon(QMessageBox.Information)
+    message.setText('<h3>You pushed the button!</h3>')
+    message.setWindowTitle('Pop up message example')
+    message.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+
+    retval = message.exec_()
+    print("value of pressed message box button:", retval)
 
 
 class PlotCanvas(FigureCanvas):
@@ -37,21 +56,23 @@ class PlotCanvas(FigureCanvas):
         self.plot()
 
     def plot(self):
-        data = np.random.randint(1, 21, (5, 5))
+        data = [2, 9, 1, 7, 4]
+        labels = ['error1', 'error2', 'error3', 'error4', 'error5']
+        x_pos = [i for i, _ in enumerate(labels)]
         ax = self.figure.add_subplot(1, 1, 1)
-        ax.plot(data, 'r-')
-        ax.set_title('PyQt Matplotlib Example')
+        ax.bar(labels, data, color="brown")
+        ax.set_title('Bar Graph')
         self.draw()
 
 
 def main():
     app = QApplication(sys.argv)
-    window = GraphWithQtDemo()
+    window = Window()
     window.setLayout(window.layout)
-    window.showMaximized()
+    window.setGeometry(50, 50, 1000, 600)
+    window.show()
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
     main()
-
