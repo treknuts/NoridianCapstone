a405c0692aa60bfbec9bbe1154e77c70415b5aea
treknuts <49407165+treknuts@users.noreply.github.com>
2020-04-22 13:56:31 -0500

    Still need to be able to filter by IoM category, position, and person.

--- detailsgeneral.py
+++ detailsgeneral.py
@@ -6,7 +6,8 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as FigureCanvas
 
 
 class DataVisualization(Frame):
-    def __init__(self, parent=None):
+    def __init__(self, parent):
+        super().__init__(parent)
         self.parent = parent
         self.background = Frame(self.parent)
         self.background.grid(row=0, column=0, sticky=NSEW)
@@ -32,29 +33,43 @@ class DataVisualization(Frame):
             top_auditors.insert(END, "{}             {}".format(a[0], a[1]))
 
         # Filter options
+        def get_review_level(combo):
+            print(combo.get())
+
+        review_label = LabelFrame(self.background)
+        review_label.grid(row=0, column=1)
         review_levels = ["Supervisor Review", "Manager Review", "Director Review", "Inter-office Review", "Technical/Quality Assurance Review"]
-        review_level_combo = Combobox(self.background)
+        review_level_combo = Combobox(review_label)
         review_level_combo['values'] = review_levels
         review_level_combo.grid(row=0, column=1)
 
-        # TODO: Get the date value from the start and end date filter options. Pass these values to update data.
+        review_btn = Button(review_label, text="Set Review Level", command=lambda :get_review_level(review_level_combo))
+        review_btn.grid(row=1, column=1)
 
-        #def show_date():
-            # start_date.set(start_date_select.get_date())
+        # TODO: Get the data from the input options to filter the data and display it accordingly.
 
+        def get_date(date1, date2):
+            print("Start date {}".format(date1.get_date()))
+            print("Start date {}".format(date2.get_date()))
 
-        start_label = LabelFrame(self.background, text="Start Date")
-        start_label.grid(row=1, column=1)
-        start_date_select = DateEntry(start_label, locale='en_US', date_pattern='MM/dd/yyyy')
-        start_date_select.grid(row=0, column=0)
-        # start_date_select.bind('<<DateEntrySelected>>', show_date())
+        # Filter by date
+        date_label = LabelFrame(self.background, text="Filter by Date")
+        date_label.grid(row=1, column=1)
 
-        # start_date = Label(start_label, text=show_date())
+        start_label = Label(date_label, text="Start Date")
+        start_label.grid(row=0, column=0)
 
-        end_label = LabelFrame(self.background, text="End Date")
-        end_label.grid(row=2, column=1)
-        end_date = DateEntry(end_label, locale='en_US', date_pattern='MM/dd/yyyy')
-        end_date.grid(row=0, column=0)
+        start_date = DateEntry(date_label, locale='en_US', date_pattern='MM/dd/yyyy')
+        start_date.grid(row=0, column=1)
+
+        end_label = Label(date_label, text="End Date")
+        end_label.grid(row=1, column=0)
+
+        end_date = DateEntry(date_label, locale='en_US', date_pattern='MM/dd/yyyy')
+        end_date.grid(row=1, column=1)
+
+        date_btn = Button(date_label, text="Set Dates", command=lambda: get_date(start_date, end_date))
+        date_btn.grid(row=2, column=1)
 
         self.create_graph()
 
