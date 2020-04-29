f5ffdd859b059d0debaee8dbfb283c46a3cb0191
treknuts <49407165+treknuts@users.noreply.github.com>
2020-04-28 19:34:10 -0500

    Filter by date, review, and error name work.

--- detailsgeneral.py
+++ detailsgeneral.py
@@ -17,20 +17,33 @@ class DataVisualization(Frame):
         self.parent = parent
         self.background = Frame(self.parent)
         self.background.grid(row=0, column=0, sticky=NSEW)
-        self.init_window()
-
         self.data = dv.errors
+        self.init_window()
 
     def init_window(self):
 
         # TODO: redraw the page once filter options are set
         def set_filter_options(error_name, review, date1, date2):
-            if error_name is not None:
-                print(error)
-            if review is not None:
+            print(error_name)
+            if error_name != "" and review is not None:
+                data_by_error = dv.get_errors_by_error_type(self.data, error_name)
+                data_by_date = dv.get_errors_by_review_date(data_by_error, date1, date2)
+                print(data_by_error)
                 print(review)
-                # Get the new data
-                data_by_date = dv.get_errors_by_review_date(dv.errors, date1, date2)
+                self.line_frame.after(100, self.line_frame.destroy())
+                self.create_graph(data_by_date)
+
+                self.pie_frame.after(100, self.pie_frame.destroy())
+                self.show_num_error(data_by_date)
+            elif error_name != "":
+                data_by_error = dv.get_errors_by_error_type(self.data, error_name)
+                data_by_date = dv.get_errors_by_review_date(data_by_error, date1, date2)
+                self.pie_frame.after(100, self.pie_frame.destroy())
+                self.show_num_error(data_by_date)
+            elif review is not None and review != "None":
+                # Get the new dates if any
+                # The reason we filter by date regardless is because the DateEntry Widget can never be None.
+                data_by_date = dv.get_errors_by_review_date(self.data, date1, date2)
                 data_by_review = dv.get_errors_by_review_type(data_by_date, review)
                 # Destroy graph to update with new data
                 self.line_frame.after(100, self.line_frame.destroy())
@@ -38,23 +51,18 @@ class DataVisualization(Frame):
                 self.create_graph(data_by_review)
 
                 self.reviewer_frame.after(100, self.reviewer_frame.destroy())
-                create_top_five(dv.errors, review)
+                create_top_five(data_by_review, review)
                 self.pie_frame.after(100, self.pie_frame.destroy())
-                self.create_pie_chart(review)
-                print("hit create_pie_chart")
-            #elif review is None:
+                self.create_pie_chart(data_by_review, review)
+            elif review == "None":
                 # TODO: This is bugged. Doesn't redraw frames right
-                #self.reviewer_frame.after(100, self.reviewer_frame.destroy())
-                #create_top_five(dv.errors, None)
-                #self.pie_frame.after(100, self.pie_frame.destroy())
-                #self.create_pie_chart(None)
-            if date1 != start_date.get_date() or date2 != end_date.get_date():
-                print(date1 != start_date.get_date())
-                print(date2 != end_date.get_date())
+                self.pie_frame.after(100, self.pie_frame.destroy())
+                self.line_frame.after(100, self.line_frame.destroy())
+                self.reviewer_frame.after(100, self.reviewer_frame.destroy())
 
-                print(date1, date2)
-                data = dv.get_errors_by_review_date(dv.errors, date1, date2)
-                self.create_graph(data)
+                self.create_pie_chart(self.data)
+                self.create_graph(self.data)
+                create_top_five(self.data)
 
         def create_top_five(data, review_level=None):
             self.reviewer_frame = Frame(self.background)
@@ -70,7 +78,7 @@ class DataVisualization(Frame):
             for r in reviewers:
                 top_reviewers.insert(END, "{} -> {} points given".format(r[0], r[1]))
 
-        create_top_five(dv.errors)
+        create_top_five(self.data)
 
         # Filter options
         filter_frame = LabelFrame(self.background, height=2, text="Filter Options", font=("Courier", 16))
@@ -79,7 +87,7 @@ class DataVisualization(Frame):
         error_label = LabelFrame(filter_frame, text="Filter by a specific error")
         error_label.grid(row=0, column=0, padx=5, pady=5)
 
-        error = StringVar()
+        error = None
         entry_label = Label(error_label, text="Error code: ")
         entry_label.grid(row=0, column=0)
         error_entry = Entry(error_label, textvariable=error)
@@ -109,7 +117,7 @@ class DataVisualization(Frame):
         start_date = DateEntry(date_label, locale='en_US', date_pattern='MM/dd/yyyy')
         start_date.grid(row=0, column=1)
         # Initialize the date entry to be the oldest date in the database
-        oldest = list(dv.sort_by_date(dv.errors))[0]
+        oldest = list(dv.sort_by_date(self.data))[0]
         start_date.set_date(oldest)
 
         end_label = Label(date_label, text="End Date")
@@ -129,8 +137,8 @@ class DataVisualization(Frame):
                             )
         filter_btn.grid(row=1, column=1)
 
-        self.create_graph(dv.errors)
-        self.create_pie_chart()
+        self.create_graph(self.data)
+        self.create_pie_chart(self.data)
 
     def create_graph(self, data):
         self.line_frame = Frame(self.background)
@@ -148,15 +156,17 @@ class DataVisualization(Frame):
         self.canvas = FigureCanvas(fig, self.line_frame)
         self.canvas.get_tk_widget().grid(column=0, row=0, rowspan=2, sticky=E)
 
-    def create_pie_chart(self, review_level=None):
+    def create_pie_chart(self, data, review_level=None):
         self.pie_frame = Frame(self.background)
         self.pie_frame.grid(row=1, column=0)
         fig = Figure(figsize=(9, 6), dpi=100)
         ax = fig.add_subplot(111)
         if review_level is not None:
-            top_five = dv.get_top_five_errors(a=dv.errors, review_level=review_level)
+            top_five = dv.get_top_five_errors(a=data, review_level=review_level)
+            print(top_five)
         else:
-            top_five = dv.get_top_five_errors(dv.errors)
+            top_five = dv.get_top_five_errors(a=data)
+            print(top_five)
         labels = []
         sizes = []
         for i in range(len(top_five)):
@@ -169,6 +179,14 @@ class DataVisualization(Frame):
         canvas = FigureCanvas(fig, self.pie_frame)
         canvas.get_tk_widget().grid(column=0, row=0)
         return canvas
+
+    def show_num_error(self, data):
+        self.pie_frame = Frame(self.background)
+        self.pie_frame.grid(row=1, column=0)
+        error = data[0][12]
+        num_errors = len(data)
+        num_errors_label = Label(self.pie_frame, text="{} : {}".format(error, num_errors), font=("Courier", 24))
+        num_errors_label.grid(row=0, column=0)
 
 
 if __name__ == '__main__':
